97508d9e052a653e1a86bfee4a07fd9757b4e88a
Henrik Skog <henrikskog01@gmail.com>
2022-02-08 14:56:57 +0100

    add react-router dependency and create simple folder structure

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,3 @@
+export default function Header({ title }) {
+  return <h2 style={{ padding: '1rem 0' }}>{title}</h2>
+}
---
+++ frontend/src/pages/create.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+export default function Create() {
+  return (
+    <>
+      <Header title={'Create recipe'} />
+      <div>Ekstra innhold på siden</div>
+    </>
+  )
+}
