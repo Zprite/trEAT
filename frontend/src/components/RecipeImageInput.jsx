d5f734cb9dc5eef5fe2e3763fa78f869e3bef496
mbbirkel <mbbirkel@stud.ntnu.no>
2022-02-10 15:51:12 +0100

    WIP on file upload

--- frontend/src/components/RecipeImageInput.jsx
+++ frontend/src/components/RecipeImageInput.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/RecipeImageInput.module.css';
 
 export default function RecipeImageInput() {
+  const [image, setImage] = useState();
+  const [imageURL, setImageURL] = useState();
+
+  useEffect(() => {
+    if (image == null) return;
+    const newImageURL = null;
+    newImageURL.push(URL.createObjectURL(image));
+    setImageURL(newImageURL);
+  }, [image]);
+
+  function onImageChange(e) {
+    setImage([...e.target.files]);
+  }
+
   return (
     <div className={styles.imageInputContainter}>
-      +
+      <input type="file" accept="image/*" onChange={onImageChange} />
+      <img src={imageURL} alt="uploaded" />
     </div>
   );
 }
