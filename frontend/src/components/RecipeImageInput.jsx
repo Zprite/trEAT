a78683d55ffb2a337271496cba0ed95a6603fbf3
mbbirkel <mbbirkel@stud.ntnu.no>
2022-02-10 13:24:14 +0100

    ADD components for recipecreator. Added css

---
+++ frontend/src/components/RecipeImageInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/RecipeImageInput.module.css';
+
+export default function RecipeImageInput() {
+  return (
+
+    <div className={styles.imageInputContainter}>
+      +
+    </div>
+  );
+}
--- frontend/src/pages/create.jsx
+++ frontend/src/pages/create.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import RecipeImageInput from '../components/RecipeImageInput';
+import '../styles/globals.css';
 
 export default function Create() {
   return (
-    <>
+    <div className="centeredPageWrapper">
       <Header title="Create recipe" />
+      <div className="recipeCreator">
+        <RecipeImageInput />
+      </div>
       <div>Ekstra innhold på siden</div>
-    </>
+    </div>
   );
 }
