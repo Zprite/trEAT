f6ffb08badb57c3f1227dde817945d8691ac7bf0
mbbirkel <mbbirkel@stud.ntnu.no>
2022-02-10 13:31:15 +0100

    made empty component + stylesheet for recipetimeinput

---
+++ frontend/src/components/RecipeTimeInput.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/RecipeTimeInput.module.css';
+
+export default function RecipeTimeInput() {
+  return (
+    <div className={styles.timeInput} />
+  );
+}
