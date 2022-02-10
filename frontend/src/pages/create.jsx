adf9fad6bd10d4a0e359cc4b731d154767adeaa4
mbbirkel <mbbirkel@stud.ntnu.no>
2022-02-10 13:58:15 +0100

    commit for testing pipeline

--- frontend/src/pages/create.jsx
+++ frontend/src/pages/create.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import RecipeImageInput from '../components/RecipeImageInput';
+import RecipeTitleInput from '../components/RecipeTitleInput';
 import '../styles/globals.css';
 
 export default function Create() {
@@ -9,6 +10,7 @@ export default function Create() {
       <Header title="Create recipe" />
       <div className="recipeCreator">
         <RecipeImageInput />
+        <RecipeTitleInput />
       </div>
       <div>Ekstra innhold på siden</div>
     </div>
