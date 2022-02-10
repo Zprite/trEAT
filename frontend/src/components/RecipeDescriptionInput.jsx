056f302b95c19aee1f80b76adde3ccca12bd3965
tg <tordgh@stud.ntnu.no>
2022-02-10 13:40:53 +0100

    Added components

---
+++ frontend/src/components/RecipeDescriptionInput.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/RecipeDescriptionInput.module.css';
+
+export default function RecipeDescriptionInput() {
+  return (
+    <div className={styles.RecipeDescriptionInput} />
+  );
+}
---
+++ frontend/src/components/RecipeTitleInput.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/RecipeTitleInput.module.css';
+
+export default function RecipeTitleInput() {
+  return (
+    <div className={styles.RecipeTitleInput} />
+  );
+}
