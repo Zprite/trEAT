5d8cedf9be53d5751955e7f91900907bdc77f041
Tord Gunnar Holen <tordgh@stud.ntnu.no>
2022-02-15 17:05:08 +0100

    Resolve "Add functionality to ingredient-component"

--- frontend/src/components/RecipeIngredientsInput.jsx
+++ frontend/src/components/RecipeIngredientsInput.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/RecipeIngredientsInput.module.css';
 
 export default function RecipeIngredientsInput() {
+  const [ingredients, setIngredients] = useState([]);
+  const [description, setDescription] = useState('');
+  const [ingredientId, setIngredientId] = useState(0);
+
+  const removeIngredient = (event) => {
+    const newIngredients = ingredients.filter(
+      (ingredient) => ingredient.id !== Number(event.target.id),
+    );
+    setIngredients(newIngredients);
+  };
+
+  function handleAdd() {
+    setIngredientId(ingredientId + 1);
+    const newIngredients = ingredients.concat({ description, id: ingredientId });
+    setIngredients(newIngredients);
+    setDescription('');
+  }
+
+  function handleChange(event) {
+    if (Number(event.charCode) === 13) {
+      handleAdd();
+      return;
+    }
+    setDescription(event.target.value);
+  }
+
   return (
     <div className={styles.RecipeIngredientsInput}>
       <h2>Ingredients</h2>
       <ul id="recipeList">
-        <li />
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.id}>
+            <button type="button" id={ingredient.id} onClick={removeIngredient}>-</button>
+            {ingredient.description}
+          </li>
+        ))}
       </ul>
       <div className={styles.addEntryContainer}>
-        <button type="button"> +</button>
-        <input type="text" id="newIngredientSubmission" />
+        <button type="button" onClick={handleAdd}>+</button>
+        <input type="text" value={description} id="newIngredientSubmission" onChange={handleChange} onKeyPress={handleChange} />
       </div>
     </div>
   );
