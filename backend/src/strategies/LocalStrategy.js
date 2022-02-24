859b41c9a8ebec4ea26eb8269c449d20f7742983
mbbirkel <mbbirkel@stud.ntnu.no>
2022-02-24 18:27:52 +0100

    create jwt strategy

--- backend/src/strategies/LocalStrategy.js
+++ backend/src/strategies/LocalStrategy.js
@@ -6,6 +6,5 @@ const LocalStrategy = passportLocal.Strategy;
 
 //Called during login/sign up.
 passport.use(new LocalStrategy(User.authenticate()))
-
 //called while after logging in / signing up to set user details in req.user
 passport.serializeUser(User.serializeUser())
