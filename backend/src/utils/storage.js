bc3a9c7e1fd6e532f7cbcfd99ad3e9388e85f35f
mbbirkel <mbbirkel@stud.ntnu.no>
2022-03-01 14:41:18 +0100

    FIX: image upload is now uniform

--- backend/src/utils/storage.js
+++ backend/src/utils/storage.js
@@ -1,10 +1,11 @@
 import multer from "multer"
 import path from 'path'
+import { getPublicDirPath } from "../utils.js";
 
 // Multer storage for image-uploads.
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../public/uploads')
+        cb(null, getPublicDirPath() + "/uploads")
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
